e7ae377ec54fedc369aa25be937f1679c8619908
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-06-10 01:41:06 +0400

    3-cm

--- ReviewAspNet/ReviewAspNet/Controllers/HomeController.cs
+++ ReviewAspNet/ReviewAspNet/Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace ReviewAspNet.Controllers
         {
             return View();
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -25,6 +24,43 @@ namespace ReviewAspNet.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        // Improvizations
+        public string GetAge(int age)
+        {
+            return $"<h1> Age is {age}<h1>";
+        }
+        public string GetNameAndAge(string name, int age)
+        {
+            return $"<b>{name} is {age} years old<b>";
+        }
+        public string Square()
+        {
+            int a = Int32.Parse(Request.Params["a"]);
+            int h = Int32.Parse(Request.Params["h"]);
+
+            int s = a * h / 2;
+
+            return $"Squares area is {s}";
+        }
+        [HttpGet]
+        public ActionResult GetBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string GetBook(string title, string author)
+        {
+            return $"{title} is create {author}";
+        }
+        public string PostBook()
+        {
+            string title = Request.Form["title"];
+            string author = Request.Form["author"];
+
+            return $"<h2>{title} is create {author}</h2>";
         }
     }
 }
