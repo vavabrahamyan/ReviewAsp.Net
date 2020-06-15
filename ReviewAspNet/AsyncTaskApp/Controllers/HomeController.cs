81da21e308747dc2a4e6c2b1e88e8bad731a5959
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-06-15 20:16:38 +0400

    async-action cm

---
+++ ReviewAspNet/AsyncTaskApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using AsyncTaskApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsyncTaskApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookContext db = new BookContext();
+        public ActionResult Index()
+        {
+            IEnumerable<Book> books = db.Books.ToList();
+            ViewBag.Books = books;
+            return View();
+        }
+        //async action
+        public async Task<ActionResult> BookList()
+        {
+            IEnumerable<Book> books = await db.Books.ToListAsync();
+            ViewBag.Books = books;
+            return View("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
