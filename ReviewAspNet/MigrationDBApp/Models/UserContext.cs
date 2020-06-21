909ceffaa4a87194e749ef82a76ecd98d07fdae6
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-06-21 22:56:26 +0400

    fst cmt

---
+++ ReviewAspNet/MigrationDBApp/Models/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MigrationDBApp.Models
+{
+    public class UserContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
