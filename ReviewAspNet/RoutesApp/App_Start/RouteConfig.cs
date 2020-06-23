46f4afa3391032a29227fe5571d0a44d5fc5434f
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-06-24 00:13:42 +0400

    ~t-cmt~

--- ReviewAspNet/RoutesApp/App_Start/RouteConfig.cs
+++ ReviewAspNet/RoutesApp/App_Start/RouteConfig.cs
@@ -15,12 +15,26 @@ namespace RoutesApp
 
             routes.MapRoute(
                 name: "Default",
-                url: "My/Account",
-                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional}
+                url: "{controller}/{action}/{id}",
+                defaults: new {controller = "Home", action = "Index", id = UrlParameter.Optional},
+                constraints: new {myConstraint = new CustomConstraint("/Home/Index/16")}
             );
 
             Route newRoute = new Route("{controller}/{action}", new MvcRouteHandler());
             routes.Add(newRoute);
+        }
+    }
+    public class CustomConstraint : IRouteConstraint
+    {
+        private string uri;
+        public CustomConstraint(string uri)
+        {
+            this.uri = uri;
+        }
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName,
+                               RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return !(uri == httpContext.Request.Url.AbsolutePath);
         }
     }
 }
