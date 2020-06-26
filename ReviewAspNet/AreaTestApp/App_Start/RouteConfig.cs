070df5c687c914f60566b7e30d5972e0318d5559
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-06-26 20:14:40 +0400

    ~14

--- ReviewAspNet/AreaTestApp/App_Start/RouteConfig.cs
+++ ReviewAspNet/AreaTestApp/App_Start/RouteConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -13,11 +14,29 @@ namespace AreaTestApp
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+            Route newRoute = new Route("{controller}/{action}", new MyRouteHandler());
+            routes.Add(newRoute);
+        }
+    }
+    public class MyRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new MyHttpHandler();
+        }
+    }
+    public class MyHttpHandler : IHttpHandler
+    {
+        public bool IsReusable => false;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("<h2> A es shat gidem? </h2>");
         }
     }
 }
