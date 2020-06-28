12e818c3d4707abec46d7dd96722c92d30f5b6cc
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-06-28 23:58:10 +0400

    ~model annotation~

--- ReviewAspNet/AreaTestApp/Models/Book.cs
+++ ReviewAspNet/AreaTestApp/Models/Book.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace AreaTestApp.Models
 {
     public class Book
     {
         public int Id { get; set; }
+        [Display(Name = "Book's name")]
         public string Name { get; set; }
+        [Display(Name = "Author of book")]
         public string Author { get; set; }
+        [Display(Name = "Create Date")]
         public int  Year { get; set; }
     }
 }
